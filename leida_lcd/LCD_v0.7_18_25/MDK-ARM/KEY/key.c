bfa928a7b0ba16f705bc8d49130b4c0438a10bfc
herold <944642675@qq.com>
2022-11-21 21:13:57 +0800

    提交人:何潜 提交原因:代码优化 1、解决按键bug

--- leida_lcd/LCD_v0.7_18_25/MDK-ARM/KEY/key.c
+++ leida_lcd/LCD_v0.7_18_25/MDK-ARM/KEY/key.c
@@ -28,37 +28,32 @@ void Key_Init(void)
 uint8_t key_val = 0;
 uint8_t KEY_Scan(uint8_t mode)   /*按键扫描*/
 {
-	uint8_t tmp_sta = 0;
+	uint8_t tmp_key_val = 0;
 	if(((KEY_UP == 0)||(KEY_DOWN == 0)||(KEY_BACK == 0)||(KEY_OK == 0)) )/*有一个出发就进入扫描*/
 	{		
 		HAL_Delay(10);			//按键消抖
-		//key = 0;
 		if(KEY_UP==0)
 		{
 			key_val = UP_KEY;
-			//while(!HAL_GPIO_ReadPin(UP_GPIO_Port,UP_Pin));			//这里后面需要优化成超时
 		}
 		else if(KEY_DOWN==0)
 		{			
 			key_val = DOWN_KEY;
-			//while(!HAL_GPIO_ReadPin(DOWN_GPIO_Port,DOWN_Pin));
 		}
 		else if(KEY_BACK==0)	
 		{
 			key_val = BACK_KEY;
-			//while(!HAL_GPIO_ReadPin(BACK_GPIO_Port,BACK_Pin));
 		}
 		else if(KEY_OK==0)		
 		{
 			key_val = OK_KEY;
-			//while(!HAL_GPIO_ReadPin(OK_GPIO_Port,OK_Pin));
 		}
-		else
-		{
-			
-			tmp_sta = key_val;
-			key_val = 0;
-		}
+
 	}
-	return tmp_sta;
+	else
+	{
+		tmp_key_val = key_val;	/*等待按键全部放开,返回最后一个放开的按键*/
+		key_val = 0;
+	}
+	return tmp_key_val;
 }
